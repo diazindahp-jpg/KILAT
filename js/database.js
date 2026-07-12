966129a5f805b8babe8df932edc77e64ea5f373a
diazindahp-jpg <diazindahp@gmail.com>
2026-07-12 09:35:41 +0700

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,110 @@
+const WHO_DATABASE = {
+
+    laki: {
+
+        BB_U: [
+            {
+                umur:0,
+                median:3.3,
+                sd:0.5
+            },
+            {
+                umur:12,
+                median:9.6,
+                sd:1.0
+            },
+            {
+                umur:24,
+                median:12.2,
+                sd:1.2
+            }
+        ],
+
+
+        TB_U:[
+            {
+                umur:0,
+                median:49.9,
+                sd:2
+            },
+            {
+                umur:12,
+                median:75.7,
+                sd:3
+            },
+            {
+                umur:24,
+                median:87.1,
+                sd:3.5
+            }
+        ],
+
+
+        IMT_U:[
+            {
+                umur:0,
+                median:13.5,
+                sd:1
+            },
+            {
+                umur:24,
+                median:16,
+                sd:1.2
+            }
+        ]
+
+    },
+
+
+
+    perempuan:{
+
+        BB_U:[
+            {
+                umur:0,
+                median:3.2,
+                sd:0.5
+            },
+            {
+                umur:12,
+                median:8.9,
+                sd:1
+            },
+            {
+                umur:24,
+                median:11.5,
+                sd:1.1
+            }
+        ],
+
+
+        TB_U:[
+            {
+                umur:0,
+                median:49.1,
+                sd:2
+            },
+            {
+                umur:12,
+                median:74,
+                sd:3
+            },
+            {
+                umur:24,
+                median:85.7,
+                sd:3.5
+            }
+        ],
+
+
+        IMT_U:[
+            {
+                umur:24,
+                median:15.8,
+                sd:1.2
+            }
+        ]
+
+    }
+
+};
